57d22acc806de7ee4cd078505231010e166eafc1
Chanduky <diego.rosa.blasco@cieep.com>
2019-03-21 13:48:32 +0100

    primera version

---
+++ src/ejemplogit/Ejemplogit.java
@@ -0,0 +1,16 @@
+
+package ejemplogit;
+
+
+public class Ejemplogit {
+
+
+    public static void main(String[] args) {
+    
+        int num=2;
+        
+        System.out.println("Hola people");
+        System.out.println("El numero es"+num);
+    }
+    
+}
